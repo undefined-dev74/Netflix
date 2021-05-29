798cdebabbcb14b21a2122afc6350fca79f46a2f
JuniorBaymax <cooladitya953@gmail.com>
2021-05-29 23:57:54 +0530

    unfineshed tag #1 commit, have to finish Jumbotron design

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/macro';
+
+export const Inner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1100px;
+  flex-direction: ${({ direction }) => direction};
+  margin: auto;
+  width: 100%;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
+`;
+
+export const Container = styled.div``;
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-bottom: 8px;
+`;
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+`;
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+export const Pane = styled.div`
+  width: 50%;
+`;
